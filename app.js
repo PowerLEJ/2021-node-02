e265a56a410d7ff8669fdee2eaedfa3f7b47eeda
ej28power <ej28power@naver.com>
2021-03-13 17:00:06 +0900

    2

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const express = require('express');
-console.log(express);
-const path = require('path')
+const app = express();
+
+app.listen(3000, () => {
+    console.log('=====================');
+    console.log('http://127.0.0.1:3000');
+    console.log('=====================');
+});
+
+app.get('/', (req, res) => {
+    const myName = 'lee'
+    res.send('<h1>Hello ' + myName + ' </h1>')
+})
+
+app.get('/hello', function(req, res) {
+    res.send('<h1>Hello, Express</h1>')
+})
+
+app.get('/api', (req, res) => {
+    const id = req.query.id;
+    const users =  [
+            { id: 1, name: '홍길동', kor: 70},
+            { id: 2, name: '홍길순', kor: 75},
+            { id: 3, name: '홍길만', kor: 80},
+    ]
+    var sendUser = id ? users.filter(v => id == v.id) : [...users];
+    res.json(sendUser)
+})
